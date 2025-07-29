b31e4571c871534f03bcf3697e097c91073de938
doyle362 <philip@lvlparking.com>
2025-07-28 21:23:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
   publicDir: 'public',
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'public/index.html'),
-    },
+    emptyOutDir: true
   },
 })
